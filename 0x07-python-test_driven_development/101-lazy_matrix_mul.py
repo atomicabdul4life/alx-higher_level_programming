2a3a43b6f9348fef03ea3289e1a2a9e5fd1cf044
atomicabdul4life <abdullateefabdulkareem60@gmail.com>
2022-07-26 03:28:23 +0100

    Self driven project added

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Matrix multiplication using NumPy.
+"""
+from numpy import matmul
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiply two matrix
+    Args:
+        m_a (list): First matrix
+        m_b (list): second matrix
+    Returns:
+        The result of multiply m_a by m_b
+    """
+    return matmul(m_a, m_b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,5 @@
+## Hello, Python!
+
+---
+
+This is a python-test-driven project from AlX School of Engineering.
